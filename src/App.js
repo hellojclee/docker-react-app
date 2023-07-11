e54bff32e192081d10d70deac32a69638145d6c9
devjay88 <jarcheol88@gmail.com>
2023-07-12 05:49:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React11311134343www
         </a>
       </header>
     </div>
